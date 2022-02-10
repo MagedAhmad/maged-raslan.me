58d4d34d0667973b7c3fc195acb17506f1d08bf0
Maged Raslan <maged.ahmedr@gmail.com>
2022-02-10 14:26:31 +0200

    remove about title in about page

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -4,7 +4,9 @@
 
  <div>
     <div class="max-w-4xl mx-auto py-20 prose lg:prose-xl text-gray-100">
+        @if($page->title !== 'about')
         <h1 class="text-gray-100" style="font-size:25px !important;">{{ $page->title }}</h1>
+        @endif
         <p class="text-gray-100">{!! $page->body !!}</p>
     </div>
 </div>
